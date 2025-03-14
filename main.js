2a2d80ae7dfe23cdb01959c11862bd1c2d49bebd
krasmik@qamadness.com <efremova@qamadness.com>
2025-03-14 19:38:01 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+// const chalk = require('chalk');
+
+console.log(chalk.blue('Hello World'));
+console.log(chalk.red('Hello World'));
+console.log(chalk.red('Hello World'));
