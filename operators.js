cacfde72367978422a737ef28787fdb71118d4c7
krasmik <krasmik@qamadness.com>
2025-03-18 16:24:28 +0200

    lesson 3 code added

---
+++ operators.js
@@ -0,0 +1,120 @@
+// let number1 = 10;
+// let number2 = 0.58;
+// let number3 = -10;
+
+// console.log(typeof number2)
+
+// const a = 0.1;
+// const b = 0.2;
+// const result = a + b;
+
+// // console.log(result);
+
+// console.log((a * 100 + b * 100) / 100); 
+
+
+// console.log((a + b).toFixed(1)); 
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-10));
+// console.log(Math.ceil(5.4)); // В більшу сторону
+// console.log(Math.round(5.8)); // Математичне округлення
+// console.log(Math.floor(5.9));// В меншу сторону
+// console.log(Math.min(...[10, 20, 30, 5, 0]));
+// console.log(Math.round(Math.random() * 10) + 90);
+
+
+// let userName = 'Jonathan';
+
+// console.log(userName[userName.length - 1]);
+
+// let greeting = "Hello";
+// // greeting = 'hello';
+// // console.log(greeting); // "Hello"
+
+// console.log(greeting.length);
+
+
+// console.log(10 + 5); // Додавання -> 15
+// console.log(10 - 5); // Віднімання -> 5
+// console.log(10 * 5); // Множення -> 50
+// console.log(10 / 5); // Ділення -> 2
+// console.log(10 % 2); // Залишок -> 1
+
+
+// let counter = 5;
+// // counter = counter + 1;
+// // counter++;
+// console.log(++counter); // 6
+// console.log(counter);
+// counter--;
+// console.log(counter); // 5
+
+
+// let numberA = 10;
+// let numberB = 20;
+// let result = numberA < numberB;
+
+// console.log(numberA < numberB);
+// console.log(numberA > numberB);
+// console.log(numberA >= 10);
+// console.log(numberA <= 10);
+// console.log(result);
+
+// console.log(numberA == '10');
+// console.log(numberA === '10');
+
+
+// console.log(10 < 5 || false); // true (логічне "АБО")
+// console.log(true && true); // false (логічне "І")
+// console.log(!true); // false (логічне "НЕ")
+
+// let input = 'Oleg';
+// const name = input ?? 'David';
+// console.log(name);
+//     console.log(null ?? "значення за замовчуванням"); // "значення за
+// console.log(0 ?? 100); // 0 (не є null або undefined)
+
+
+// console.log(Boolean(1)); // true
+// console.log(Boolean(0)); // false
+// console.log(Boolean("test")); // true
+// console.log(Boolean("")); // false
+
+
+// console.log(!!1);
+
+
+// console.log(Number("42")); // 42
+// console.log(Number("238s")); // NaN
+// console.log(Number(" 32 ")); // 32
+// console.log(Number(true)); // 1
+// console.log(Number(false)); // 0
+
+// console.log(parseInt("100px")); // 100
+// console.log(parseFloat("10.5rem")); // 10.5
+// console.log(parseInt("15.6")); // 15
+// console.log(parseFloat("48.5.4")); // 48.5
+// console.log(parseInt("a123")); // NaN
+
+// console.log(String(true)); // "true"
+// console.log(String(42)); // "42"
+// console.log(String(null)); // "null"
+
+// console.log(5 + 42);
+// console.log(5 + "42"); // 542, бо + перетворить операнд на Number
+// console.log(5 + true); // 6
+// console.log(5 + "test"); // 5test, бо якщо один з операндів - строка, то + перетворює інший операнд також на строку
+// console.log("5" * "2"); //10, бо -*/% перетворюють операнди на числа
+
+// console.log(5 + "10"); // "510" (конкатенація)
+// console.log("15" - 5); // 10 (перетворення рядка в число)
+// console.log(null + 10); // 10
+// console.log(true + 1); // 2
+
+
+// console.log(1 == true);
+
+
+// console.log(false > -99)
